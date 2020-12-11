cfb0df6d28468a99e52c3e44b157aae3bae1629b
wicker <33050775+YCodeD@users.noreply.github.com>
2020-12-11 08:48:55 +0800

    Update test_email.py

--- crawler/pass_10/test_email.py
+++ crawler/pass_10/test_email.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 acount = '1255280233@qq.com'
-password = 'xnzeqyxhrlagfeif'
+password = '***'
 receiver = '1255280233@qq.com'
 
 mailhost = 'smtp.qq.com'
